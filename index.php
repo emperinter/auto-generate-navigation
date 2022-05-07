e065780728b8431d12fb4e84ad981660a1f704fd
emperinter <hehiuzhang@gmail.com>
2022-05-07 16:37:29 +0800

    unique name and change error information

--- index.php
+++ index.php
@@ -1,45 +1,48 @@
 <?php
+/*
+ * Plugin Name:       Auto Generate Navigation 
+ * Plugin URI:        https://github.com/emperinter/auto-generate-navigation
+ * Description:       A plugin which can generate a navigation catalogue on your WordPress post!（文章目录生成器！）
+ * Version:           1.2
+ * Requires at least: 4.6
+ * Requires PHP:      5.6
+ * Author:            emperinter
+ * Author URI:        https://www.emperinter.info/about/
+ * License:           GNU General Public License v2.0
+ * License URI:       https://github.com/emperinter/auto-generate-navigation/blob/master/LICENSE
+ * Update URI:        https://github.com/emperinter/auto-generate-navigation
+ */
+
 /**
  * @package Auto Generate Navigation
- * @version 1.1
+ * @version 1.2
  * 
  *  From: https://github.com/yaohaixiao/autocjs
  *  License： MIT License 
  * 
  */
+//CDN Resource Host
+//https://yaohaixiao.github.io/autocjs/css/autoc.css
+//https://cdn.jsdelivr.net/gh/yaohaixiao/autocjs@2.0.1/src/css/autoc.css
+//https://cdn.jsdelivr.net/gh/yaohaixiao/autocjs/dist/autoc.min.js
 
-/*
-Plugin Name: Auto Generate Navigation 
-Plugin URI: https://github.com/emperinter/Auto-Generate-Navigation
-Description: A plugin which can generate a navigation catalogue on your WordPress post!（文章目录生成器！）
-Author: emperinter
-Version: 1.1
-Author URI: https://www.emperinter.info
-*/
-
-//		CDN Resource Host
-//      https://yaohaixiao.github.io/autocjs/css/autoc.css
-//      https://cdn.jsdelivr.net/gh/yaohaixiao/autocjs@2.0.1/src/css/autoc.css
-//      https://cdn.jsdelivr.net/gh/yaohaixiao/autocjs/dist/autoc.min.js
-
-
-function add_styles() {
+function auto_generate_navigate_styles() {
 	wp_register_style('plugin_stylesheet_css', plugins_url('autoc.css', __FILE__));
 	wp_enqueue_style('plugin_stylesheet_css');
 	wp_register_script('plugin_stylesheet_js', plugins_url('autoc.min.js', __FILE__));
 	wp_enqueue_script('plugin_stylesheet_js');
 }
 
-add_action( 'wp_enqueue_scripts', 'add_styles' );  
+add_action( 'wp_enqueue_scripts', 'auto_generate_navigate_styles' );  
 
 
-function addJs(){
+function auto_generate_navigate_js(){
 	echo"
 		<script>
 			let navigation = new AutocJs({
 				article: '#AgnArticle',
 				selector: 'h1,h2,h3,h4,h5,h6',
-				title: '文章目录导航',
+				title: 'Navigation',
 				position: 'outside',
 				anchorURL: '',
 				anchorAt: 'front',
@@ -58,15 +61,15 @@ function addJs(){
 }
 
 
-function addIDBeforeContent($content){
+function auto_generate_navigate_add_content_id($content){
 	return '<div id="AgnArticle">' . $content . '</div>';
 }
 
-function add(){	
-	add_filter('wp_footer', 'addJs');
-	add_filter('the_content', 'addIDBeforeContent');
+function auto_generate_navigate_add(){	
+	add_filter('wp_footer', 'auto_generate_navigate_js');
+	add_filter('the_content', 'auto_generate_navigate_add_content_id');
 }
 
 if(!is_admin() and !is_category() and !is_archive()){ 
-	add();
+	auto_generate_navigate_add();
 }
--- readme.txt
+++ readme.txt
@@ -3,9 +3,9 @@ Contributors: emperinter
 Tags: Navigation
 Donate link: https://www.emperinter.info/wp-content/uploads/2020/09/1599052795-PayPal.webp
 Requires at least: 4.6
-Tested up to: 7.4
-Requires PHP: 4.6
-Stable tag: 1.1
+Tested up to: 5.9.3
+Requires PHP: 5.6
+Stable tag: 1.2
 License: GNU General Public License v2.0
 License URI: https://github.com/emperinter/Auto-Generate-Navigation/blob/master/LICENSE
 
@@ -18,19 +18,21 @@ A plugin which  can generate a navigation on your post!
 一个可以在你的WordPress文章上生成导航目录的插件!
 
 == Installation ==
-You can find it here:https://github.com/emperinter/Auto-Generate-Navigation
+You can find it here: https://github.com/emperinter/auto-generate-navigation
 
 == Frequently Asked Questions ==
 1. how to install？
-You can find it here: https://github.com/emperinter/Auto-Generate-Navigation
+You can find it here: https://github.com/emperinter/auto-generate-navigation
 
 2. 如何安装？
-你可以在这里找到: https://github.com/emperinter/Auto-Generate-Navigation/blob/master/READEME_cn.md
+你可以在这里找到: https://github.com/emperinter/auto-generate-navigation/blob/master/READEME_cn.md
 
 == Screenshots ==
 screenshot-1.png：样式截图
 
 == Changelog ==
+=== 1.2 ===
+fix error as Requires
 === 1.1 ===
 fix load stylesheet
 === 1.0 ===
